0c9585fda3a07d0e4113563ea811a4ce3674a58c
Dick Veldkamp <d.veldkamp@mzh.nl>
2022-12-16 09:01:29 +0100

    Weergegevens Groningen nu uit het bericht te peuteren

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Iets van een webproject. Nog geen idee wat. 
 Nog even uitzoeken hoe we de venv automatisch meestarten. 
 
+- Gekozen voor een json afkomstig van buienradar
+- kijken of we daar de gegevens van een individueel station kunnen lospeuteren.
--- webapp.py
+++ webapp.py
@@ -1,8 +1,21 @@
 import requests
 
 
-url = "https://jsonplaceholder.typicode.com/todos/1"
+url = "https://data.buienradar.nl/2.0/feed/json"
 
 response = requests.request("GET",url)
+actueel = response.json()["actual"]
+lokaal = actueel["stationmeasurements"]
+exacteLocatie = lokaal[12]              #voor nu ff zo. Groningen is dit.
 
-print(response.json()['title'])
+
+print(actueel["sunrise"])
+print(actueel["sunset"])
+
+print(exacteLocatie["stationname"])
+print(exacteLocatie["timestamp"])
+print(exacteLocatie["weatherdescription"])
+print(exacteLocatie["airpressure"])
+print(exacteLocatie["temperature"])
+print(exacteLocatie["groundtemperature"])
+print(exacteLocatie["windspeed"])
